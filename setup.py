6fc32d5c3638d8b088b6023812d0142d9620a72f
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2024-04-20 13:50:01 +0545

    data ingestion

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-# HYPEN_E_DOT='-e .'
+HYPEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
-
 setup(
-
-name="Xray",
-version="0.0.1",
-author="Rajnish Adhikari",
-author_email="rajnishadhikari01@gmail.com",
-install_requires=get_requirements(r"D:\Data Science\5th_April Deep Learinig Project\requirements_dev.txt"),
-package=find_packages()
-
+    name="Xray",
+    version="0.0.1",
+    author="Rajnish Adhikari",
+    author_email="rajnishadhikari01@gmail.com",
+    install_requires=get_requirements(r"D:\\Data Science\\5th_April Deep Learinig Project\\requirements_dev.txt"),
+    packages=find_packages()
 )
